4b165300f26832908d0309e394e1fe7017e7d367
swaroop-nath <swaroopnath6@gmail.com>
2019-05-28 19:00:07 +0530

    Feature Set enlarged to accomodate dimensions of elements. Data gathered into 5 excel sheet.

--- data_gathering_script.py
+++ data_gathering_script.py
@@ -2,6 +2,8 @@ import urllib.request as request
 from bs4 import BeautifulSoup
 import re
 from pandas import DataFrame, ExcelWriter
+from selenium import webdriver
+import itertools
 
 # A tempporary URL to test the working of the feature extraction algorithm
 url = 'https://stackoverflow.com/questions/13336576/extracting-an-information-from-web-page-by-machine-learning'
@@ -16,8 +18,26 @@ children_ratio = []  #This ratio signifies the sparsity of children tags in the 
 id_relevance_extent = []  #Integer Value - Possible Values: 0, 1, 2, 3; this sees if the id/class/role attribute has a value from the accepted words, if it does, then the check is done for how many words from the accepted list match the value
 tag_main = []  #Boolean value - Possible Values: 'Yes' and 'No'
 tag_article = []  #Boolean value - Possible Values: 'Yes' and 'No'
+class_value = []
+coord_x = []
+coord_y = []
+height = []
+width = []
+element_area_ratio = []
 
 regexp = '; |, | |\n|-\*'
+
+def xpath_soup(element):
+    components = []
+    child = element if element.name else element.parent
+    for parent in child.parents:
+        previous = itertools.islice(parent.children, 0, parent.contents.index(child))
+        xpath_tag = child.name
+        xpath_index = sum(1 for i in previous if i.name == xpath_tag) + 1
+        components.append(xpath_tag if xpath_index == 1 else '%s[%d]' % (xpath_tag, xpath_index))
+        child = parent
+    components.reverse()
+    return '/%s' % '/'.join(components)
 
 def findRelevanceExtent(id_class_role):
     relevance_extent = 0
@@ -36,7 +56,7 @@ def findRelevanceExtent(id_class_role):
         flag_2 = False
     return relevance_extent
 
-def featureExtraction(soup):
+def featureExtraction(soup, driver, root_area):
     divs = soup.body.find_all('div')
     for div in divs:
         tag_h.append(len(div.find_all('h1')) + len(div.find_all('h2')) + len(div.find_all('h3')) + len(div.find_all('h4')) + len(div.find_all('h5')) + len(div.find_all('h6')))
@@ -59,20 +79,51 @@ def featureExtraction(soup):
             tag_article.append('NO')
         id_class_role = [div.get('id'), div.get('class'), div.get('role')]
         id_relevance_extent.append(findRelevanceExtent(id_class_role))
+        flag_class = False
+        for x in id_class_role:
+            if not x is None:
+                flag_class = True
+                class_value.append(x)
+                break
+        if not flag_class: class_value.append('nan')
+        x_path_div = xpath_soup(div)
+        element = driver.find_element_by_xpath(x_path_div)
+        coord_x.append(element.location.get('x'))
+        coord_y.append(element.location.get('y'))
+        temp_h = element.size.get('height')
+        temp_w = element.size.get('width')
+        if temp_h is None: height.append('nan')
+        else: height.append(temp_h)
+        if temp_w is None: width.append('nan')
+        else: width.append(temp_w)
+        if temp_h is None or temp_w is None: element_area_ratio.append('0')
+        else: element_area_ratio.append(temp_h*temp_w/root_area)
 
         
 def formCSVData():
-    data = {'tag_h': tag_h,'tag_p': tag_p,'tag_formatting': tag_formatting,'tag_table': tag_table,'word_count': word_count,'children_ratio': children_ratio,'id_relevance_extent': id_relevance_extent,'tag_main': tag_main,'tag_article': tag_article}
+    data = {'tag_h': tag_h,'tag_p': tag_p,'tag_formatting': tag_formatting,'tag_table': tag_table,'word_count': word_count,'children_ratio': children_ratio,'id_relevance_extent': id_relevance_extent,'tag_main': tag_main,'tag_article': tag_article,'x': coord_x,'y': coord_y,'height': height,'width': width,'element_area_ratio': element_area_ratio ,'class_value': class_value}
+    # print('tag_h:' + str(len(tag_h)) + ', coord_x: ' + str(len(coord_x)) + ', coord_y: ' + str(len(coord_y)) + ', height: ' + str(len(height)) + ', width: ' + str(len(width)) + ', element_are_ratio: ' + str(len(element_area_ratio)))
     # col_names = ['tag_h1', 'tag_h2', 'tag_h3', 'tag_h4', 'tag_h5', 'tag_h6', 'tag_p', 'tag_b', 'tag_i', 'tag_u', 'tag_em', 'tag_small', 'tag_strike', 'tag_li', 'tag_ol', 'tag_ul', 'tag_table', 'word_count', 'children_ratio', 'id_relevance_extent', 'tag_main', 'tag_article']
     df = DataFrame(data)
-    writer = ExcelWriter('data_gathered.xlsx', engine = 'xlsxwriter')
-    df.to_excel(writer, sheet_name = 'Sheet1')
+    writer = ExcelWriter('data_gathered_part_5.xlsx', engine = 'xlsxwriter')
+    df.to_excel(writer, sheet_name = 'Sheet1', index = False, header = True)
     writer.save()
 
-def extractFrom(content):
+def extractFrom(content, URI):
     soup_object = BeautifulSoup(content, 'lxml')
-    featureExtraction(soup_object)
+    driver = webdriver.Firefox(executable_path=r'D:\Softwares\Anaconda\Anaconda\geckodriver-v0.24.0-win64\geckodriver.exe')
+    # print(URI)
+    driver.get(URI)
+    root_x_path = xpath_soup(soup_object.body)
+    root_element = driver.find_element_by_xpath(root_x_path)
+    # print(root_element.size)
+    root_area = root_element.size.get('width') * root_element.size.get('height')
+    # print(root_area)
+    # print(type(root_area))
+    featureExtraction(soup_object, driver, root_area)
     formCSVData()
+    driver.quit()
+    # root_area = 0
 
 
 # Driver Program to test the functions
