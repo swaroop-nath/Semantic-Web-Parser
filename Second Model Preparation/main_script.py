972b74c3db266fb738ab392675dc070969b80e5d
swaroop-nath <swaroopnath6@gmail.com>
2019-07-04 22:32:12 +0530

    Faulty cases have been analysed and some inferences have been drawn on the feature list.

--- Second Model Preparation/main_script.py
+++ Second Model Preparation/main_script.py
@@ -28,54 +28,75 @@ dataset['tag_sup_child'] = encoder.fit_transform(dataset['tag_sup_child'].values
 dataset['red_flag_id'] = encoder.fit_transform(dataset['red_flag_id'].values)
 
 #Transforming the variables
-
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+data = dataset.iloc[:, :-2]
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
 
 X[:, -1] = np.cbrt(X[:,-1])
-smote = SMOTENC(random_state = 42, categorical_features = [0,1,2,4,9,10,11,12,13,14,15])
+#smote = SMOTENC(random_state = 42, categorical_features = [0,1,2,4,9,10,11,12,13,14,15])
 
 #Splitting the data into train-validation-test
 X_aux, X_test, y_aux, y_test = train_test_split(X, y, random_state = 42, test_size = 0.2)
-#X_train, X_validation, y_train, y_validation = train_test_split(X_aux, y_aux, test_size = 0.2)
+X_train, X_validation, y_train, y_validation = train_test_split(X_aux, y_aux, random_state = 42, test_size = 0.2)
 
-recall_scores_knn = []
-recall_scores_knn_t = []
-recall_scores_dtc = []
-recall_scores_dtc_t = []
-recall_scores_rfc = []
-recall_scores_rfc_t = []
+#recall_scores_knn = []
+#recall_scores_knn_t = []
+#recall_scores_dtc = []
+#recall_scores_dtc_t = []
+#recall_scores_rfc = []
+#recall_scores_rfc_t = []
+#
+#cv = KFold(n_splits=10, random_state=42, shuffle=False)
+#
+#for train_index, test_index in cv.split(X_aux):
+#    X_train, X_validation, y_train, y_validation = X_aux[train_index], X_aux[test_index], y_aux[train_index], y_aux[test_index]
+#
+#    X_train, y_train = smote.fit_resample(X_train, y_train)
+#    knn_classifier = KNNC(X_train, y_train)
+#    y_pred_knn = knn_classifier.predictValues(X_validation)
+#    y_pred_knn_t = knn_classifier.predictValues(X_train)
+#    dtc_classifier = DTC(X_train, y_train)
+#    y_pred_dtc = dtc_classifier.predictValues(X_validation)
+#    y_pred_dtc_t = dtc_classifier.predictValues(X_train)
+#    rfc_classifier = RFC(X_train, y_train)
+#    y_pred_rfc = rfc_classifier.predictValues(X_validation)
+#    y_pred_rfc_t = rfc_classifier.predictValues(X_train)
+#    
+#    matrix_knn = confusion_matrix(y_validation, y_pred_knn)
+#    matrix_knn_t = confusion_matrix(y_train, y_pred_knn_t)
+#    
+#    matrix_dtc = confusion_matrix(y_validation, y_pred_dtc)
+#    matrix_dtc_t = confusion_matrix(y_train, y_pred_dtc_t)
+#    
+#    matrix_rfc = confusion_matrix(y_validation, y_pred_rfc)
+#    matrix_rfc_t = confusion_matrix(y_train, y_pred_rfc_t)
+#    
+#    recall_scores_knn.append(matrix_knn[1,1]/sum(matrix_knn[1]))
+#    recall_scores_knn_t.append(matrix_knn_t[1,1]/sum(matrix_knn_t[1]))
+#    
+#    recall_scores_dtc.append(matrix_dtc[1,1]/sum(matrix_dtc[1]))
+#    recall_scores_dtc_t.append(matrix_dtc_t[1,1]/sum(matrix_dtc_t[1]))
+#    
+#    recall_scores_rfc.append(matrix_rfc[1,1]/sum(matrix_rfc[1]))
+#    recall_scores_rfc_t.append(matrix_rfc_t[1,1]/sum(matrix_rfc_t[1]))
 
-cv = KFold(n_splits=10, random_state=42, shuffle=False)
+def findEquality(i, x):
+    if X_validation[x, -1] == dataset.height_width_diff[i] and X_validation[x, 5] == dataset.relative_x_coord[i] and X_validation[x, 6] == dataset.relative_y_coord[i]: return True
+    else: return False
 
-for train_index, test_index in cv.split(X_aux):
-    X_train, X_validation, y_train, y_validation = X_aux[train_index], X_aux[test_index], y_aux[train_index], y_aux[test_index]
+def findFaultyCases(y_true, y_pred):
+    faulty_indices = []
+    faulty_cases = []
+    for i in range(len(y_pred)):
+        if y_true[i] != y_pred[i]: faulty_indices.append(i)
+    print(len(faulty_indices))
+    for x in faulty_indices:
+        for i in range(dataset.shape[0]):
+            if findEquality(i, x): faulty_cases.append(i)
+        
+    return faulty_cases
 
-    X_train, y_train = smote.fit_resample(X_train, y_train)
-    knn_classifier = KNNC(X_train, y_train)
-    y_pred_knn = knn_classifier.predictValues(X_validation)
-    y_pred_knn_t = knn_classifier.predictValues(X_train)
-    dtc_classifier = DTC(X_train, y_train)
-    y_pred_dtc = dtc_classifier.predictValues(X_validation)
-    y_pred_dtc_t = dtc_classifier.predictValues(X_train)
-    rfc_classifier = RFC(X_train, y_train)
-    y_pred_rfc = rfc_classifier.predictValues(X_validation)
-    y_pred_rfc_t = rfc_classifier.predictValues(X_train)
-    
-    matrix_knn = confusion_matrix(y_validation, y_pred_knn)
-    matrix_knn_t = confusion_matrix(y_train, y_pred_knn_t)
-    
-    matrix_dtc = confusion_matrix(y_validation, y_pred_dtc)
-    matrix_dtc_t = confusion_matrix(y_train, y_pred_dtc_t)
-    
-    matrix_rfc = confusion_matrix(y_validation, y_pred_rfc)
-    matrix_rfc_t = confusion_matrix(y_train, y_pred_rfc_t)
-    
-    recall_scores_knn.append(matrix_knn[1,1]/sum(matrix_knn[1]))
-    recall_scores_knn_t.append(matrix_knn_t[1,1]/sum(matrix_knn_t[1]))
-    
-    recall_scores_dtc.append(matrix_dtc[1,1]/sum(matrix_dtc[1]))
-    recall_scores_dtc_t.append(matrix_dtc_t[1,1]/sum(matrix_dtc_t[1]))
-    
-    recall_scores_rfc.append(matrix_rfc[1,1]/sum(matrix_rfc[1]))
-    recall_scores_rfc_t.append(matrix_rfc_t[1,1]/sum(matrix_rfc_t[1]))
+rfc_classifier = RFC(X_train, y_train)
+y_pred = rfc_classifier.predictValues(X_validation)
+faults = findFaultyCases(y_validation, y_pred)
+matrix = confusion_matrix(y_validation, y_pred)
